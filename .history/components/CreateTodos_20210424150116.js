b54b5dbfa76842c16e1dc69f23b9841f4be334b5
shamroz73 <shamrozwarraich@gmail.com>
2021-04-24 15:01:59 +0200

    bug fix

--- .history/components/CreateTodos_20210424150116.js
+++ .history/components/CreateTodos_20210424150116.js
@@ -8,6 +8,8 @@ export default function CreateTodos() {
   const { todos, creatTodos } = useTodos();
   const { user } = useUser();
 
+  console.log(todos);
+  //
   const addTodolistner = async (e) => {
     e.preventDefault();
     await creatTodos({ descriptions: newTodo });
